47e4f2aba5436e0d8a58734cf4148d20e139538d
alxtim10 <alxtim10@gmail.com>
2026-05-08 20:31:59 +0700

    feat:add favicon and tab title

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,12 @@ import "./globals.css";
 import { Toaster } from "sonner";
 import MobileSidebar from "@/components/layout/mobile-sidebar";
 import { Poppins } from "next/font/google";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "SIMUDA Dashboard",
+  description: "SIMUDA Dashboard",
+};
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -27,7 +33,7 @@ export default function RootLayout({
             <div className="md:hidden flex items-center justify-between p-4 border-b bg-white sticky top-0 z-40">
               <MobileSidebar />
 
-              <h1 className="font-semibold">Youth CMS</h1>
+              <h1 className="font-semibold">SIMUDA Dashboard</h1>
             </div>
 
             <div className="max-w-7xl mx-auto p-5 lg:p-8 xl:p-10">
